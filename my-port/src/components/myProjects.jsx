9a9adabfad6c410ecac1582f51ed23833f26970e
mohammed-rharbi <rharbi383@gmail.com>
2024-12-18 23:34:08 +0100

    Add new myProjects component

---
+++ my-port/src/components/myProjects.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import Projects from './projects'
+const MyProjects = () => {
+
+
+  return (
+
+
+
+<section className='px-24 py-24 '>
+
+    <Projects/>
+
+</section>
+
+  );
+};
+
+export default MyProjects;
