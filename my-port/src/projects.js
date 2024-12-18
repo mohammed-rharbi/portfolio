6d477ee61eb219d4e0bd65fa4abe2b177243609b
mohammed-rharbi <rharbi383@gmail.com>
2024-12-18 23:34:08 +0100

    Add new projects data file

---
+++ my-port/src/projects.js
@@ -0,0 +1,168 @@
+export const projects = [
+
+    {
+        name: 'RegiSport',
+        description: 'RegiSport is a platform for managing and organizing sports events and activities.',
+        technologies: ['NodeJs', 'Mongo', 'React' , 'Nest' ,'Tailwind'],
+        link: 'https://github.com/mohammed-rharbi/RegiSport',
+        demo: 'https://yourdemourl.com/mongrandtaxi',
+        image: '/sporti.avif'
+    },
+
+    {
+        name: 'CinéManager',
+        description: 'CinéManager is a platform for managing and organizing movie screenings and events.',
+        technologies: ['NodeJs', 'Mongo', 'React' , 'Tailwind','Express'],
+        link: 'https://github.com/mohammed-rharbi/cinema_manger',
+        demo: 'https://yourdemourl.com/mongrandtaxi',
+        image: '/cin.png'
+    },
+
+    {
+        name: 'My Gym',
+        description: 'platform for gym entusiast to manage their gym membership and access to the gym facilities and classes and also to book classes and facilities ',
+        technologies: ['JavaScript', 'SQL', 'Laravel', 'React' , 'Tailwind'],
+        link: 'https://github.com/mohammed-rharbi/MY-GYM',
+        demo: 'https://yourdemourl.com/mongrandtaxi',
+        image: '/myGym.png'
+    },
+
+    {
+      name: 'Evento',
+      description: 'Evento is creating a platform for event management and reservations. Users can book and generate tickets, while organizers manage their events.',
+      technologies: ['SQL', 'JavaScript','Laravel','PHP' ,'Tailwind'],
+      link: 'https://github.com/mohammed-rharbi/evento-app',
+      demo: 'https://yourdemourl.com/evento',
+      image: '/Evento.png'
+    },
+  
+    {
+      name: 'MonGrandTaxi',
+      description: 'MonGrandTaxi is a user-friendly platform enabling users to find available drivers and drivers to publish their availability',
+      technologies: ['JavaScript', 'SQL', 'Laravel', 'Tailwind','PHP'],
+      link: 'https://github.com/mohammed-rharbi/My-Cab',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/electro-maroc-17-65c0988f5116d694298253.png'
+    },
+   
+    {
+        name: 'Cinematy SIRH',
+        description: 'Cinematy SIRH is a system that facilitate the management of employees, applications, calls for tenders, and includes advanced features such as OCR detection for automatic document processing.',
+        technologies: ['NodeJs', 'Mongo',  'Next' , 'Nest' ,'Tailwind'],
+        link: 'https://github.com/mohammed-rharbi/EmployeeManger',
+        demo: 'https://yourdemourl.com/mongrandtaxi',
+        image: '/RH.jpeg'
+    },
+
+    {
+        name: 'Instant Communication Service',
+        description: 'Instant Communication Service is a platform for instant communication between users with real-time notifications and messaging features also with video and audio calls',
+        technologies: ['NodeJs', 'Mongo', 'React' , 'Nest' ,'Bootstrap'],
+        link: 'https://github.com/mohammed-rharbi/JScommunication',
+        demo: 'https://yourdemourl.com/mongrandtaxi',
+        image: '/WATCH.png'
+    },
+
+    {
+        name: 'Watch Together',
+        description: 'watch together is a platform for watching movies and series together with your friends and family in real-time and with a chat feature',
+        technologies: ['NodeJs', 'Mongo', 'React' , 'Nest','Socket' ,'Bootstrap'],
+        link: 'https://github.com/mohammed-rharbi/AsyncPlay',
+        demo: 'https://yourdemourl.com/mongrandtaxi',
+        image: '/COM.jpeg'
+    },
+
+    {
+      name: 'Smart Exam',
+      description: 'is an application intended for educational establishments, training centers, and companies wishing to assess the skills of their learners or employees.',
+      technologies: ['NodeJs', 'SQL', 'React','EJS' ,'Bootstrap'],
+      link: 'https://github.com/mohammed-rharbi/JSSmartExam',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/smart.webp'
+    },
+
+    {
+      name: 'Wiki To',
+      description: 'Wiki To is an efficient content management system, combined with a front office, to provide an exceptional user experience.',
+      technologies: ['JavaScript', 'SQL', 'PHP', 'Bootstrap'],
+      link: 'https://github.com/mohammed-rharbi/WIKI_TO',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/Wiki.jpeg'
+    },
+
+    {
+        name: 'Recipe Hub',
+        description: 'Recipe Hub is a platform for sharing and discovering recipes with a community of food enthusiasts.',
+        technologies: ['JavaScript', 'SQL', 'Laravel', 'Bootstrap'],
+        link: 'https://github.com/mohammed-rharbi/Recipes_Hub',
+        demo: 'https://yourdemourl.com/mongrandtaxi',
+        image: '/recpies.png'
+    },
+
+    {
+      name: 'Digital Menu Board',
+      description: 'Digital Menu is a SaaS Platform that offer an intuitive experience to restaurant owners. Create, customize and share your digital menus with ease, while benefiting from secure authentication, real-time email notifications and optimized performance.',
+      technologies: ['JavaScript', 'React' , 'SQL', 'Laravel', 'Bootstrap'],
+      link: 'https://github.com/mohammed-rharbi/Digital-Menu',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/digitalMenu.png'
+    },
+
+    {
+      name: 'Smart Travel',
+      description: 'smart travel is a revolutionary travel startup dedicated to simplifying the bus booking process and elevating the entire travel experience.',
+      technologies: ['JavaScript', 'SQL', 'PHP', 'Bootstrap'],
+      link: 'https://github.com/mohammed-rharbi/SmartTravel',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/smart-Travel.png'
+    },
+
+    {
+      name: 'Digital Bizcard',
+      description: 'platform for digital business card management using Laravel Sanctum for authentication and access policies to allow users to create, edit and delete their own business cards',
+      technologies: ['JavaScript', 'SQL', 'Laravel', 'Bootstrap'],
+      link: 'https://github.com/mohammed-rharbi/bizcard',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/bizcard.png'
+    },
+
+    {
+      name: 'Book Verse',
+      description: 'platform that  allow users to quickly access information about their favorite books',
+      technologies: ['JavaScript','SQL', 'Laravel', 'React' ],
+      link: 'https://github.com/mohammed-rharbi/BookVerse',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/BookVerse.png'
+    },
+
+    {
+      name: 'Electro Nacer',
+      description: 'Electro Nacer is a platform and a website for a company that sells electrical supplies and appliances',
+      technologies: ['JavaScript', 'SQL', 'PHP','HTML' , 'React' ],
+      link: 'https://github.com/mohammed-rharbi/ELECTRO-NACER',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/NACER.png'
+    },
+
+    {
+      name: 'Fashion Hub',
+      description: 'FashionHub is more than just a clothing website. It is a place where fashion enthusiasts can explore the latest trends and styles in an engaging and inspiring way',
+
+      technologies: ['JavaScript', 'HTML' , 'css' ],
+      link: 'https://github.com/mohammed-rharbi/fashion-hub',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/FushionHub.png'
+    },
+
+    {
+      name: 'Galerie Artistique',
+      description: 'Galerie Artistique is a platform for artists to showcase their work and for art enthusiasts to explore and purchase art pieces',
+      technologies: ['JavaScript', 'HTML' , 'css' ],
+      link: 'https://github.com/mohammed-rharbi/galerie-artistique',
+      demo: 'https://yourdemourl.com/mongrandtaxi',
+      image: '/ArtAgency.png'
+    },
+
+];
+
+
